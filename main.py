6b515d476395bd2e71a2a18be5741fc15a10f4b0
jason.inbody <jasoninbody@jason.inbody>
2022-01-17 14:52:39 -0800

    django launches in the background

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import subprocess
 import os
+from threading import Thread
+from django.core.management import execute_from_command_line
+from kivy.app import App
+
 os.environ['DJANGO_SETTINGS_MODULE'] = 'backend.settings'
 
 from kivy.lang import Builder
@@ -7,7 +11,6 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import ScreenManager, NoTransition, Screen
 from kivy.clock import Clock   
-from kivymd.app import MDApp
 from jnius import autoclass
 from android.runnable import run_on_ui_thread
 
@@ -15,30 +18,44 @@ WebView = autoclass('android.webkit.WebView')
 WebViewClient = autoclass('android.webkit.WebViewClient')
 activity = autoclass('org.kivy.android.PythonActivity').mActivity
 
+from kivy.utils import platform
+if platform == "android":
+    from android.permissions import request_permissions, Permission
+    request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
+
+
 Builder.load_string("""
 <HomeScreen>
     BoxLayout:
-        orientation: "vertical"
-        Button:
-            id: status_box
-            size_hint_x: 0.7
-            text: "Hello world"
-    AnchorLayout:
-        anchor_x: "right"
-        anchor_y: "bottom"
-        padding: dp(20), dp(20)
-        MDFillRoundFlatButton:
-            text: "hello world"
+        Widget:
+        BoxLayout:
+            orientation: "vertical"
+            Widget:
+            Label:
+                id: status_box
+                size_hint_x: 0.7
+                text: "Hello world"
+            Widget:
+        Widget:
 """)
 
 
-class KivyDjango(MDApp):
+@run_on_ui_thread
+def create_webview(*args):
+    webview = WebView(activity)
+    webview.getSettings().setJavaScriptEnabled(True)
+    wvc = WebViewClient()
+    webview.setWebViewClient(wvc)
+    activity.setContentView(webview)
+    webview.loadUrl('http://127.0.0.1:8000/')
+
+
+class KivyDjango(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.theme_cls.primary_palette = "Teal"
 
     def build(self):
-        subprocess.call(['python', './backend/manage.py', 'runserver'])
+        self.run_django_thread()
         self.mainbox = FloatLayout()
         self.screens = AnchorLayout(anchor_x='center', anchor_y='center')
 
@@ -47,17 +64,14 @@ class KivyDjango(MDApp):
         self.content.add_widget(HomeScreen(name="home"))
         self.screens.add_widget(self.content)
         self.mainbox.add_widget(self.screens)
-        Clock.schedule_once(self.create_webview, 0)
+        Clock.schedule_once(create_webview, 5)
         return self.mainbox
 
-    @run_on_ui_thread
-    def create_webview(self, *args):
-        webview = WebView(activity)
-        webview.getSettings().setJavaScriptEnabled(True)
-        wvc = WebViewClient();
-        webview.setWebViewClient(wvc);
-        activity.setContentView(webview)
-        webview.loadUrl('http://127.0.0.1:8000/')
+    def run_django_thread(self):
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+        thread = Thread(target=execute_from_command_line, args=(["python", "runserver", "--noreload"],))
+        thread.start()
+
 
 
 class HomeScreen(Screen):
